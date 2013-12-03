5830bfea1fac6eca32e57c7052c25855fac4dcbb
Jey <geetan123321@gmail.com>
2013-12-03 15:11:52 +0530

    New rails app without activerecord

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Live4Awesomeness::Application.config.secret_key_base = 'e2068823efc3ab0027d6cee64d18fbba0b0271bd98859b528a44d3f694bafa87f3e91f1ee147d9fff34b59d64a53456168f0825b508425fbd7d879314aae96ee'
